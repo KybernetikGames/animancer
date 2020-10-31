e415995426d7f4c895566e94da123b709b40717e
Kailas <kailasdierk@gmail.com>
2020-10-31 12:50:46 +1100

    Improved the `Generate Animations By Sprite Name` function to support multi-object selection of both `Sprite`s and `Texture`s.

--- docs/manual/getting/sprites/AnimancerEditorUtilities.Sprites.cs
+++ docs/manual/getting/sprites/AnimancerEditorUtilities.Sprites.cs
@@ -5,7 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,46 +17,171 @@ namespace Animancer.Editor
     {
         /************************************************************************************************************************/
 
-        [MenuItem("CONTEXT/TextureImporter/Generate Animations By Sprite Name")]
-        private static void GenerateAnimationsBySpriteName(MenuCommand command)
+        private const string GenerateAnimationsBySpriteNameFunctionName = "/Generate Animations By Sprite Name";
+
+        /************************************************************************************************************************/
+
+        [MenuItem("Assets/Create/Animancer" + GenerateAnimationsBySpriteNameFunctionName, validate = true)]
+        private static bool ValidateGenerateAnimationsBySpriteName()
+        {
+            var selection = Selection.objects;
+            for (int i = 0; i < selection.Length; i++)
+            {
+                var selected = selection[i];
+                if (selected is Sprite || selected is Texture)
+                    return true;
+            }
+
+            return false;
+        }
+
+        [MenuItem("Assets/Create/Animancer" + GenerateAnimationsBySpriteNameFunctionName, priority = Strings.AssetMenuOrder + 13)]
+        private static void GenerateAnimationsBySpriteName()
+        {
+            var sprites = new List<Sprite>();
+
+            var selection = Selection.objects;
+            for (int i = 0; i < selection.Length; i++)
+            {
+                var selected = selection[i];
+                if (selected is Sprite sprite)
+                {
+                    sprites.Add(sprite);
+                }
+                else if (selected is Texture)
+                {
+                    var path = AssetDatabase.GetAssetPath(selected);
+                    var assets = AssetDatabase.LoadAllAssetsAtPath(path);
+                    for (int j = 0; j < assets.Length; j++)
+                    {
+                        sprite = assets[j] as Sprite;
+                        if (sprite != null)
+                            sprites.Add(sprite);
+                    }
+                }
+            }
+
+            GenerateAnimationsBySpriteName(sprites);
+        }
+
+        /************************************************************************************************************************/
+
+        private static List<Sprite> _CachedSprites;
+
+        /************************************************************************************************************************/
+
+        [MenuItem("CONTEXT/" + nameof(Sprite) + GenerateAnimationsBySpriteNameFunctionName)]
+        private static void GenerateAnimationsFromSpriteByName(MenuCommand command)
+        {
+            // Delay the call in case multiple objects are selected.
+            if (_CachedSprites == null)
+            {
+                _CachedSprites = new List<Sprite>();
+                EditorApplication.delayCall += () =>
+                {
+                    GenerateAnimationsBySpriteName(_CachedSprites);
+                    _CachedSprites = null;
+                };
+            }
+
+            _CachedSprites.Add((Sprite)command.context);
+        }
+
+        /************************************************************************************************************************/
+
+        [MenuItem("CONTEXT/" + nameof(TextureImporter) + GenerateAnimationsBySpriteNameFunctionName, validate = true)]
+        private static bool ValidateGenerateAnimationsFromTextureBySpriteName(MenuCommand command)
         {
             var importer = (TextureImporter)command.context;
             var assets = AssetDatabase.LoadAllAssetsAtPath(importer.assetPath);
-            var name = Path.GetFileNameWithoutExtension(importer.assetPath).Replace(" ", "");
+            for (int i = 0; i < assets.Length; i++)
+            {
+                if (assets[i] is Sprite)
+                    return true;
+            }
 
-            var pathPrefix = Path.GetDirectoryName(importer.assetPath) + "/";
-            pathPrefix += name + "-";
+            return false;
+        }
+
+        [MenuItem("CONTEXT/" + nameof(TextureImporter) + GenerateAnimationsBySpriteNameFunctionName)]
+        private static void GenerateAnimationsFromTextureBySpriteName(MenuCommand command)
+        {
+            // Delay the call in case multiple objects are selected.
+            if (_CachedSprites == null)
+            {
+                _CachedSprites = new List<Sprite>();
+                EditorApplication.delayCall += () =>
+                {
+                    GenerateAnimationsBySpriteName(_CachedSprites);
+                    _CachedSprites = null;
+                };
+            }
+
+            var importer = (TextureImporter)command.context;
+            var assets = AssetDatabase.LoadAllAssetsAtPath(importer.assetPath);
+            for (int i = 0; i < assets.Length; i++)
+            {
+                if (assets[i] is Sprite sprite)
+                    _CachedSprites.Add(sprite);
+            }
+        }
+
+        /************************************************************************************************************************/
+
+        private static void GenerateAnimationsBySpriteName(List<Sprite> sprites)
+        {
+            if (sprites.Count == 0)
+                return;
+
+            sprites.Sort((a, b) => EditorUtility.NaturalCompare(a.name, b.name));
 
             var nameToSprites = new Dictionary<string, List<Sprite>>();
 
-            Array.Sort(assets, (a, b) => a.name.CompareTo(b.name));
-            for (int i = 0; i < assets.Length; i++)
+            for (int i = 0; i < sprites.Count; i++)
             {
-                var sprite = assets[i] as Sprite;
-                if (sprite == null)
-                    continue;
+                var sprite = sprites[i];
 
-                // Remove spaces and numbers.
-                var baseName = Regex.Replace(sprite.name, @"[ \d-]", "");
+                // Remove numbers from the end.
+                var baseName = sprite.name.TrimEnd(' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+                //Regex.Replace(sprite.name, @"\d+$", "");
 
-                if (!nameToSprites.TryGetValue(baseName, out var sprites))
+                if (!nameToSprites.TryGetValue(baseName, out var spriteGroup))
                 {
-                    sprites = new List<Sprite>();
-                    nameToSprites.Add(baseName, sprites);
+                    spriteGroup = new List<Sprite>();
+                    nameToSprites.Add(baseName, spriteGroup);
                 }
 
-                sprites.Add(sprite);
+                // Add the sprite to the group if it's not a duplicate.
+                if (spriteGroup.Count == 0 || spriteGroup[spriteGroup.Count - 1] != sprite)
+                    spriteGroup.Add(sprite);
             }
 
-            foreach (var sprites in nameToSprites)
+            var pathToSprites = new Dictionary<string, List<Sprite>>();
+
+            var message = new StringBuilder()
+                .Append("Do you wish to generate the following animations?");
+            var line = 0;
+            foreach (var nameToSpriteGroup in nameToSprites)
             {
-                var animationName = sprites.Key;
-                if (animationName.StartsWith(name))
-                    animationName = animationName.Substring(name.Length, animationName.Length - name.Length);
-                animationName = pathPrefix + animationName + ".anim";
+                var path = AssetDatabase.GetAssetPath(nameToSpriteGroup.Value[0]);
+                path = Path.GetDirectoryName(path);
+                path = Path.Combine(path, nameToSpriteGroup.Key + ".anim");
+                pathToSprites.Add(path, nameToSpriteGroup.Value);
 
-                CreateAnimation(animationName, sprites.Value.Count, sprites.Value.ToArray());
+                if (++line < 30)
+                    message.AppendLine()
+                        .Append("- ")
+                        .Append(path)
+                        .Append(" (")
+                        .Append(nameToSpriteGroup.Value.Count)
+                        .Append(" frames)");
             }
+
+            if (!EditorUtility.DisplayDialog("Generate Sprite Animations?", message.ToString(), "Generate", "Cancel"))
+                return;
+
+            foreach (var pathToSpriteGroup in pathToSprites)
+                CreateAnimation(pathToSpriteGroup.Key, pathToSpriteGroup.Value.Count, pathToSpriteGroup.Value.ToArray());
 
             AssetDatabase.SaveAssets();
         }
