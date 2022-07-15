c2d20e373bb1e8743d2962d28f386608713ec906
Kailas Dierk <kailasdierk@gmail.com>
2022-07-15 19:49:10 +1000

    Changed all GIFs to be paused on their first frame by default and you can click to play/stop them.

---
+++ assets/js/pngif.js
@@ -0,0 +1,15 @@
+const collection = document.getElementsByClassName("pngif");
+for (let i = 0; i < collection.length; i++) {
+  var item = collection[i];
+  item.setAttribute("onclick", "pngif(this);");
+}
+
+function pngif(item) {
+  var src = item.getAttribute("src");
+  var name = src.slice(0, -3);
+  if (src[src.length - 1] == "g")
+    name += "gif";
+  else
+    name += "png";
+  item.setAttribute("src", name);
+}
