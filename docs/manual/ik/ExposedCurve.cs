794c27c4031c60c292a75d87d5c9ce719ecbca19
Kailas Dierk <kailasdierk@gmail.com>
2024-08-25 10:57:58 +1000

    Cleaned up the End Events page.

--- docs/manual/ik/ExposedCurve.cs
+++ docs/manual/ik/ExposedCurve.cs
@@ -1,4 +1,4 @@
-﻿// Animancer // Copyright 2020 Kybernetik //
+﻿// Animancer // https://kybernetik.com.au/animancer // Copyright 2018-2024 Kybernetik //
 
 using System;
 using UnityEngine;
@@ -6,8 +6,8 @@ using UnityEngine;
 namespace Animancer
 {
     /// <summary>
-    /// A <see cref="ScriptableObject"/> which extracts a particular <see cref="AnimationCurve"/> from an
-    /// <see cref="AnimationClip"/> in the Unity Editor so that it can be accessed at runtime.
+    /// A <see cref="ScriptableObject"/> which extracts a particular <see cref="AnimationCurve"/>
+    /// from an <see cref="AnimationClip"/> in the Unity Editor so that it can be accessed at runtime.
     /// </summary>
     [CreateAssetMenu(menuName = Strings.MenuPrefix + "Exposed Curve", order = Strings.AssetMenuOrder + 9)]
     public class ExposedCurve : ScriptableObject
@@ -23,9 +23,7 @@ namespace Animancer
             get => _Clip;
             set
             {
-                if (value != null)
-                    Validate.NotLegacy(value);
-
+                Validate.AssertAnimationClip(value, false, $"set {nameof(ExposedCurve)}.{nameof(Clip)}");
                 _Clip = value;
             }
         }
@@ -44,11 +42,8 @@ namespace Animancer
         private string _PropertyName;
 
         /// <summary>[Editor-Only] [<see cref="SerializeField"/>] The name of the curve to extract.</summary>
-        public string PropertyName
-        {
-            get => _PropertyName;
-            set => _PropertyName = value;
-        }
+        public ref string PropertyName
+            => ref _PropertyName;
 #endif
 
         /************************************************************************************************************************/
@@ -73,37 +68,31 @@ namespace Animancer
 
         /// <summary>Returns the <see cref="Curve"/>.</summary>
         /// <exception cref="NullReferenceException">Thrown if the `curve` is null.</exception>
-        public static implicit operator AnimationCurve(ExposedCurve curve) => curve.Curve;
+        public static implicit operator AnimationCurve(ExposedCurve curve)
+            => curve.Curve;
 
         /************************************************************************************************************************/
 
         /// <summary>
-        /// Returns the value of the <see cref="Curve"/> at the current <see cref="AnimancerState.Time"/> of the state
-        /// registered with the <see cref="Clip"/> as its key.
+        /// Returns the value of the <see cref="Curve"/> at the current <see cref="AnimancerState.Time"/>
+        /// of the state registered with the <see cref="Clip"/> as its key.
         /// </summary>
         public float Evaluate(AnimancerComponent animancer)
-        {
-            return Evaluate(animancer.States[_Clip]);
-        }
+            => Evaluate(animancer.States[_Clip]);
 
         /// <summary>
         /// Returns the value of the <see cref="Curve"/> at the current <see cref="AnimancerState.Time"/>.
         /// </summary>
         public float Evaluate(AnimancerState state)
-        {
-            if (state == null)
-                return 0;
-
-            return Evaluate(state.Time % state.Length);
-        }
+            => state == null
+            ? 0
+            : Evaluate(state.Time % state.Length);
 
         /// <summary>
         /// Returns the value of the <see cref="Curve"/> at the specified `time`.
         /// </summary>
         public float Evaluate(float time)
-        {
-            return _Curve.Evaluate(time);
-        }
+            => _Curve.Evaluate(time);
 
         /************************************************************************************************************************/
 #if UNITY_EDITOR
@@ -111,8 +100,7 @@ namespace Animancer
 
         protected virtual void OnEnable()
         {
-            UnityEditor.EditorCurveBinding binding;
-            if (TryGetCurveBinding(out binding))
+            if (TryGetCurveBinding(out UnityEditor.EditorCurveBinding binding))
             {
                 _Curve = UnityEditor.AnimationUtility.GetEditorCurve(_Clip, binding);
             }
@@ -124,7 +112,7 @@ namespace Animancer
 
         /************************************************************************************************************************/
 
-        private void OnValidate()
+        protected virtual void OnValidate()
         {
             OnEnable();
         }
@@ -136,7 +124,7 @@ namespace Animancer
             if (_Clip != null &&
                !string.IsNullOrEmpty(_PropertyName))
             {
-                var bindings = UnityEditor.AnimationUtility.GetCurveBindings(_Clip);
+                UnityEditor.EditorCurveBinding[] bindings = UnityEditor.AnimationUtility.GetCurveBindings(_Clip);
                 for (int i = 0; i < bindings.Length; i++)
                 {
                     binding = bindings[i];
